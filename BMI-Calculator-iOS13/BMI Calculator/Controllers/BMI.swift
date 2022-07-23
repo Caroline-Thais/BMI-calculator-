5e35d6b95ee0abc0c4aa7dbe33f299b021aed574
Thais Caroline Silva <76595905+Caroline-Thais@users.noreply.github.com>
2022-07-23 19:05:45 -0300

    Add files via upload

---
+++ BMI-Calculator-iOS13/BMI Calculator/Controllers/BMI.swift
@@ -0,0 +1,17 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Thaís Caroline Silva on 23/07/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI{
+    
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+}
